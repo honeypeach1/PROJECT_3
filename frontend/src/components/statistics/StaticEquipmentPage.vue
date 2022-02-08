a7a342b42c42027a219b7e97b084e59cc0c32147
jamey <sir91@naver.com>
2022-02-08 17:37:54 +0900

    악취 & 통계 페이지 UI&UX 기본 골격 제작 완료 및 설계 진행중

--- frontend/src/components/statistics/StaticEquipmentPage.vue
+++ frontend/src/components/statistics/StaticEquipmentPage.vue
@@ -1,31 +1,30 @@
 <template>
+  <div id="body-wrapper">
+    <div id="header">
+      <main-header/>
+    </div>
+    <div id="staticPage">
+      <!--장비 맵 영역-->
+      <div class="staticLeftArea showMap" id="staticMapArea">
+        <div id="staticMap"></div>
+      </div>
+      <!--장비 셀렉트 영역-->
+      <div class="staticLeftArea showList" id="staticListArea">
+      </div>
 
-  <div id="header">
-    <main-header/>
+      <!--장비 데이터 조회 영역-->
+      <div id="staticCenterArea">
+        <div id="selectDateArea">기간 설정 영역</div>
+        <div id="showGraphArea">그래프 영역</div>
+        <div id="showChartArea">그래프 영역</div>
+      </div>
+      <!--풍배도 및 빈도 표현 영역-->
+      <div id="staticRightArea">
+        <div id="dataControlArea">데이터 조회</div>
+        <div id="showWindRoseArea">풍배도 영역</div>
+      </div>
+    </div>
   </div>
-
-  <div id="staticPage">
-    <!--장비 맵 영역-->
-    <div class="staticLeftArea showMap" id="staticMapArea">
-      <div id="staticMap"></div>
-    </div>
-    <!--장비 셀렉트 영역-->
-    <div class="staticLeftArea showList" id="staticListArea">
-    </div>
-
-    <!--장비 데이터 조회 영역-->
-    <div id="staticCenterArea">
-      <div id="selectDateArea">기간 설정 영역</div>
-      <div id="showGraphArea">그래프 영역</div>
-      <div id="showChartArea">그래프 영역</div>
-    </div>
-    <!--풍배도 및 빈도 표현 영역-->
-    <div id="staticRightArea">
-      <div id="dataControlArea">데이터 조회</div>
-      <div id="showWindRoseArea">풍배도 영역</div>
-    </div>
-  </div>
-
 </template>
 
 <script>
@@ -50,17 +49,17 @@ export default {
   },
   mounted() {
     if (window.kakao && window.kakao.maps) {
-      this.initMap();
+      this.initStaticMap();
     } else {
       const script = document.createElement("script");
       /* global kakao */
-      script.onload = () => kakao.maps.load(this.initMap);
+      script.onload = () => kakao.maps.load(this.initStaticMap);
       script.src = "//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=" + KAKAO_API_KEY + "&libraries=services";
       document.head.appendChild(script);
     }
   },
   methods: {
-    initMap() {
+    initStaticMap() {
       const container = document.getElementById("staticMap");
       const options = {
         center: new kakao.maps.LatLng(33.450701, 126.570667),
